a47267d0abd0fc1e3268b52db457432ad416911d
ignaciozanotti <ignacio.zanotti@gmail.com>
2020-04-27 17:29:16 -0300

    eslint added

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ let didShowMessage = false;
 
 function showMessage()
 {
-    const message = "I like logs";
+	const message = "I like logs";
 
-    for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < 5; i++) {
+		//eslint-disable-next-line
         console.log(message);
         
-    }
+		//eslint-disable-next-line semi
+		console.log(message)
+        
+	}
 }
 
 showMessage();
